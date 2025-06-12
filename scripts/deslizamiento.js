3a5c6be841dc61889e539c3b5b5a5f1e65953b55
2Diego2 <diegoabelleyra74@gmail.com>
2025-06-12 12:05:42 -0300

    Se agrega lo que se trabajo el dia 09062025. 1-Se agrego en la parte de proyectos, 4 tarjetas interactivas con distintos proyectos. 2-Se agrego un icono a las paginas. 3-Se agrego fotos y informacion a todas las paginas.

--- scripts/deslizamiento.js
+++ scripts/deslizamiento.js
@@ -1,3 +1,4 @@
+    // Deslizamiento suave para la navegación
     $(function(){
       $.scrollify({
         section : "section",
@@ -39,3 +40,37 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+
+
+//Movimiento de las imágenes del slider 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
